e26336921d4905a3d4dc179c19d34dac0e663e10
Lucas Parry <lucas@envato.com>
2013-01-21 11:42:49 +1100

    Style changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,16 @@
 # Manages scoutapp.com agent
-class scout($scout_key=false, $user="root") {
-  package {
-    'scout':
-      ensure   => 'installed',
-      provider => 'gem';
+class scout(
+  $scout_key,
+  $user
+) {
+  package { 'scout':
+    ensure   => 'installed',
+    provider => 'gem';
   }
 
   # set up the cronjob that runs the agent every minute
-  if $scout_key {
-    cron {
-      'scout':
-        user    => $user,
-        command => "/var/lib/gems/1.8/bin/scout ${scout_key}";
-    }
+  cron { 'scout':
+    user    => $user,
+    command => "/usr/bin/env scout ${scout_key}";
   }
 }
--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,3 +1,4 @@
+# Installs the ruby mysql client library required by some scout plugins
 class scout::mysql {
   # Install ruby mysql library using mysql module.
   include mysql::client::ruby
