073315796204de0de4912525f2516b4849f67f8b
AJ <sevaine@gmail.com>
2014-03-31 16:02:46 +1100

    make module more self-sufficient
    - create scout user if missing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,31 @@
 # Manages scoutapp.com agent
 class scout(
-  $scout_key,
-  $user,
+  $scout_key        = undef,
+  $user             = 'scout',
   $cron_environment = undef,
-  $home_dir = undef,
-  $public_cert = undef,
+  $home_dir         = '/home/scout',
+  $public_cert      = undef,
 ) {
+
+  if $public_cert {
+    $scout_cert_path = "${home_dir}/.scout"
+
+    file { $scout_cert_path:
+      ensure  => directory,
+      owner   => $user,
+      require => User[$user],
+    }
+
+    file { "${scout_cert_path}/scout_rsa.pub":
+      content => $public_cert,
+      owner => $user,
+      require => [
+        File[$scout_cert_path],
+        User[$user],
+      ],
+    }
+  }
+
   package { ['ruby','rubygems']:
     ensure  => present,
   }
@@ -19,23 +39,17 @@ class scout(
     ],
   }
 
-  # set up the cronjob that runs the agent every minute
   cron { 'scout':
+    require     => User[$user],
     user        => $user,
     command     => "/usr/bin/env scout ${scout_key}",
     environment => $cron_environment,
   }
 
-  # Install the public cert if defined so we can install custom plugins
-  if $public_cert {
-    if $home_dir {
-      $scout_cert_path = "${home_dir}/.scout"
-    } else {
-      $scout_cert_path = "/home/${user}/.scout"
-    }
-    file { "${scout_cert_path}/scout_rsa.pub":
-      content => $public_cert,
-      owner => $user,
-    }
+  user { $user:
+    ensure      => present,
+    managehome  => true,
+    homedir     => $home_dir,
   }
+
 }
