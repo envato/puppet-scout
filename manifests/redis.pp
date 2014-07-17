20f665538d9e80a5213e547ff1c6149d90f1142a
Andrew Humphrey <andrew.humphrey@envato.com>
2014-07-18 08:38:54 +1000

    Fix class comment

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -1,4 +1,4 @@
-# Installs the ruby mysql client library required by some scout plugins
+# Installs the redis client library required by some scout plugins
 class scout::redis {
   package { 'redis':
     ensure    => present,
