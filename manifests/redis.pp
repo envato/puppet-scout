65a4718e1072e97bdff19e5da267dbc1910f6ed8
Andrew Humphrey <andrew.humphrey@envato.com>
2014-08-21 09:17:46 +1000

    Check to make sure we don't redefine packages
    redis and sidekiq are both packages likely to be defined in other
    manifests, make sure that we don't cause a redefinition error

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -1,11 +1,13 @@
 # Installs the redis client library required by some scout plugins
 class scout::redis {
-  package { 'redis':
-    ensure    => present,
-    provider  => 'gem',
-    require   => [
-      Package['ruby'],
-      Package['rubygems'],
-    ],
+  if (!defined(Package['redis'])) {
+    package { 'redis':
+      ensure    => present,
+      provider  => 'gem',
+      require   => [
+        Package['ruby'],
+        Package['rubygems'],
+      ],
+    }
   }
 }
