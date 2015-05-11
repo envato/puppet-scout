595eedf126987479ce31f85c65ab791e9f0e7817
Den Mat <dennis.matotek@envato.com>
2015-05-11 11:15:03 +1000

    Splitting user into it's own file

---
+++ manifests/user.pp
@@ -0,0 +1,28 @@
+class scout::user (
+  $ensure    = 'present',
+  $user      = 'scout',
+  $homedir   = '/home/scout',
+  group      = 'scout',
+  managehome = true,
+  groups     = undef
+  ) {
+
+  validate_absolute_path($homedir)
+
+  group { $group:
+    ensure => $ensure,
+  }
+
+  if is_array($groups) {
+    User[$user] { groups => $groups }
+  } else {
+    User[$user] { groups = any2array($groups)
+  }
+
+  user { $user:
+    group      => $group,
+    home       => $homedir,
+    managehome => $managehome,
+    require    => Group[$group],
+  }
+}
