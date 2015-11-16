2956f69462566766bdc2b77418b2cdedd49bb82a
DenMat <dennis.matotek@envato.com>
2015-11-16 16:50:09 +1100

    get them around the right way

--- spec/classes/cron_spec.rb
+++ spec/classes/cron_spec.rb
@@ -6,22 +6,22 @@ describe 'scout::cron' do
     should contain_class('scout::cron')
   end
 
-  context 'with scout_roles' do
-    it 'should install cron with roles' do
+  context 'without scout_roles' do
+    it 'should install cron without roles' do
       should contain_cron('scout').with(
         'ensure'  => 'present',
-        'command' => '/usr/bin/env scout blah -e blah -r arole,brole,crole',
+        'command' => '/usr/bin/env scout blah -e blah',
         'user'    => 'scout'
       )
     end
   end
 
-  context 'without scout_roles' do
+  context 'with scout_roles' do
     let(:facts) { { :special => 'special' } }
-    it 'should install cron without roles' do
+    it 'should install cron with roles' do
       should contain_cron('scout').with(
         'ensure'  => 'present',
-        'command' => '/usr/bin/env scout blah -e blah',
+        'command' => '/usr/bin/env scout blah -e blah -r arole,brole,crole',
         'user'    => 'scout'
       )
     end
