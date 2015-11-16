4c7f4c37a59387e70fe1ebe3f050a2493198d817
DenMat <dennis.matotek@envato.com>
2015-11-16 17:43:47 +1100

    set environment to undef so as not to make everything production.
      * also updated tests to match.

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -3,7 +3,7 @@ class scout::cron (
   $ensure                 = 'present',
   $user                   = scout,
   $scout_key              = undef,
-  $scout_environment_name = 'production',
+  $scout_environment_name = undef,
   $cron_environment       = undef,
   ) {
 
@@ -13,17 +13,22 @@ class scout::cron (
     Cron['scout'] { environment => $cron_environment }
   }
 
+  if $scout_environment_name {
+    $add_env_to_cmd = "/usr/bin/env scout ${scout_key} -e ${scout_environment_name}"
+  } else {
+    $add_env_to_cmd = "/usr/bin/env scout ${scout_key}"
+  }
   if is_array($scout_roles) {
     $roles = join($scout_roles, ',')
-    $cmd = "/usr/bin/env scout ${scout_key} -e ${scout_environment_name} -r ${roles}"
+    $final_cmd = "${add_env_to_cmd} -r ${roles}"
   } else {
-    $cmd = "/usr/bin/env scout ${scout_key} -e ${scout_environment_name}"
+    $final_cmd = "${add_env_to_cmd}"
   }
 
   cron { 'scout':
     ensure  => $ensure,
     user    => $user,
-    command => $cmd,
+    command => $final_cmd,
     require => User[$user],
   }
 }
--- spec/classes/cron_spec.rb
+++ spec/classes/cron_spec.rb
@@ -6,8 +6,19 @@ describe 'scout::cron' do
     should contain_class('scout::cron')
   end
 
-  context 'without scout_roles' do
-    it 'should install cron without roles' do
+  context 'without scout_roles or scout_environment_name' do
+    it 'should install cron without roles or environment' do
+      should contain_cron('scout').with(
+        'ensure'  => 'present',
+        'command' => '/usr/bin/env scout blah',
+        'user'    => 'scout'
+      )
+    end
+  end
+
+  context 'with just environment set' do
+    let(:params) { { :scout_environment_name => 'blah' } }
+    it 'should install with only environment' do
       should contain_cron('scout').with(
         'ensure'  => 'present',
         'command' => '/usr/bin/env scout blah -e blah',
@@ -16,12 +27,23 @@ describe 'scout::cron' do
     end
   end
 
-  context 'with scout_roles' do
+  context 'with scout_roles and environment' do
     let(:facts) { { :special => 'special' } }
-    it 'should install cron with roles' do
+    it 'should install cron with roles and environment' do
       should contain_cron('scout').with(
         'ensure'  => 'present',
         'command' => '/usr/bin/env scout blah -e blah -r arole,brole,crole',
+        'user'    => 'scout'
+      )
+    end
+  end
+
+  context 'with scout_roles and no environment' do
+    let(:facts) { { :special => 'more_special' } }
+    it 'should install cron with roles' do
+      should contain_cron('scout').with(
+        'ensure'  => 'present',
+        'command' => '/usr/bin/env scout blah -r arole,brole,crole',
         'user'    => 'scout'
       )
     end
